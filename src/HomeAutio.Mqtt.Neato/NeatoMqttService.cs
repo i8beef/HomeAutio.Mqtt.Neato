78e865f96e3c8b763f314852e721f05f3e242dac
Michael Hallock <mike@hallock.net>
2019-08-18 16:43:20 -0400

    Factored out some interfaces

---
+++ src/HomeAutio.Mqtt.Neato/NeatoMqttService.cs
@@ -0,0 +1,254 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using HomeAutio.Mqtt.Core;
+using I8Beef.Neato;
+using I8Beef.Neato.Commands;
+using I8Beef.Neato.Events;
+using Microsoft.Extensions.Logging;
+using MQTTnet;
+
+namespace HomeAutio.Mqtt.Neato
+{
+    /// <summary>
+    /// Neato MQTT Service.
+    /// </summary>
+    public class NeatoMqttService : ServiceBase
+    {
+        private ILogger<NeatoMqttService> _log;
+        private bool _disposed = false;
+
+        private IClient _client;
+        private string _NeatoName;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NeatoMqttService"/> class.
+        /// </summary>
+        /// <param name="logger">Logging instance.</param>
+        /// <param name="NeatoClient">Neato client.</param>
+        /// <param name="NeatoName">Neato name.</param>
+        /// <param name="brokerSettings">MQTT broker settings.</param>
+        public NeatoMqttService(
+            ILogger<NeatoMqttService> logger,
+            IClient NeatoClient,
+            string NeatoName,
+            BrokerSettings brokerSettings)
+            : base(logger, brokerSettings, "Neato/" + NeatoName)
+        {
+            _log = logger;
+            SubscribedTopics.Add(TopicRoot + "/controls/+/set");
+
+            _client = NeatoClient;
+            _NeatoName = NeatoName;
+
+            _client.EventReceived += Neato_EventReceived;
+
+            // Neato client logging
+            _client.MessageSent += (object sender, MessageSentEventArgs e) => { _log.LogInformation("Neato Message sent: " + e.Message); };
+            _client.MessageReceived += (object sender, MessageReceivedEventArgs e) => { _log.LogInformation("Neato Message received: " + e.Message); };
+            _client.Error += (object sender, System.IO.ErrorEventArgs e) =>
+            {
+                var exception = e.GetException();
+                _log.LogError(exception, exception.Message);
+                System.Environment.Exit(1);
+            };
+        }
+
+        #region Service implementation
+
+        /// <inheritdoc />
+        protected override async Task StartServiceAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            _client.Connect();
+            await GetConfigAsync()
+                .ConfigureAwait(false);
+        }
+
+        /// <inheritdoc />
+        protected override Task StopServiceAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return Task.CompletedTask;
+        }
+
+        #endregion
+
+        #region MQTT Implementation
+
+        /// <summary>
+        /// Handles commands for the Neato published to MQTT.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        protected override async void Mqtt_MqttMsgPublishReceived(object sender, MqttApplicationMessageReceivedEventArgs e)
+        {
+            var message = e.ApplicationMessage.ConvertPayloadToString();
+            _log.LogInformation("MQTT message received for topic " + e.ApplicationMessage.Topic + ": " + message);
+
+            var commandType = e.ApplicationMessage.Topic.Replace(TopicRoot + "/controls/", string.Empty).Replace("/set", string.Empty);
+
+            Command command = null;
+            if (commandType == "power")
+                command = new PowerCommand { Value = message };
+
+            if (commandType == "volume")
+                command = new VolumeCommand { Value = message };
+
+            if (commandType == "mute")
+                command = new MuteCommand { Value = message };
+
+            if (commandType == "input")
+                command = new InputCommand { Value = message };
+
+            if (commandType == "surroundMode")
+                command = new SurroundModeCommand { Value = message };
+
+            if (commandType == "tunerFrequency")
+                command = new TunerFrequencyCommand { Value = message };
+
+            if (commandType == "tunerMode")
+                command = new TunerModeCommand { Value = message };
+
+            if (commandType.StartsWith("zone"))
+            {
+                if (int.TryParse(commandType.Substring(4, 1), out int zoneId))
+                {
+                    if (commandType == $"zone{zoneId}Power")
+                        command = new ZonePowerCommand { Value = message, ZoneId = zoneId };
+
+                    if (commandType == $"zone{zoneId}Volume")
+                        command = new ZoneVolumeCommand { Value = message, ZoneId = zoneId };
+
+                    if (commandType == $"zone{zoneId}Mute")
+                        command = new ZoneMuteCommand { Value = message, ZoneId = zoneId };
+
+                    if (commandType == $"zone{zoneId}Input")
+                        command = new ZoneInputCommand { Value = message, ZoneId = zoneId };
+                }
+            }
+
+            if (command != null)
+            {
+                await _client.SendCommandAsync(command)
+                    .ConfigureAwait(false);
+            }
+        }
+
+        #endregion
+
+        #region Neato implementation
+
+        /// <summary>
+        /// Handles publishing updates to the Neato current activity to MQTT.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private async void Neato_EventReceived(object sender, CommandEventArgs e)
+        {
+            _log.LogInformation($"Neato event received: {e.Command.GetType()} {e.Command.Code} {e.Command.Value}");
+
+            string commandType = null;
+            switch (e.Command.GetType().Name)
+            {
+                case "PowerCommand":
+                    commandType = "power";
+                    break;
+                case "VolumeCommand":
+                    commandType = "volume";
+                    break;
+                case "MuteCommand":
+                    commandType = "mute";
+                    break;
+                case "InputCommand":
+                    commandType = "input";
+                    break;
+                case "SurroundModeCommand":
+                    commandType = "surroundMode";
+                    break;
+                case "TunerFrequencyCommand":
+                    commandType = "tunerFrequency";
+                    break;
+                case "TunerModeCommand":
+                    commandType = "tunerMode";
+                    break;
+                case "ZonePowerCommand":
+                    commandType = $"zone{((ZonePowerCommand)e.Command).ZoneId}Power";
+                    break;
+                case "ZoneVolumeCommand":
+                    commandType = $"zone{((ZoneVolumeCommand)e.Command).ZoneId}Volume";
+                    break;
+                case "ZoneMuteCommand":
+                    commandType = $"zone{((ZoneMuteCommand)e.Command).ZoneId}Mute";
+                    break;
+                case "ZoneInputCommand":
+                    commandType = $"zone{((ZoneInputCommand)e.Command).ZoneId}Input";
+                    break;
+            }
+
+            if (commandType != null)
+            {
+                await MqttClient.PublishAsync(new MqttApplicationMessageBuilder()
+                        .WithTopic(TopicRoot + "/controls/" + commandType)
+                        .WithPayload(e.Command.Value)
+                        .WithAtLeastOnceQoS()
+                        .WithRetainFlag()
+                        .Build())
+                        .ConfigureAwait(false);
+            }
+        }
+
+        /// <summary>
+        /// Maps Neato device actions to subscription topics.
+        /// </summary>
+        /// <returns>An awaitable <see cref="Task"/>.</returns>
+        private async Task GetConfigAsync()
+        {
+            var commands = new Command[]
+            {
+                new PowerCommand { Value = "?" },
+                new VolumeCommand { Value = "?" },
+                new MuteCommand { Value = "?" },
+                new InputCommand { Value = "?" },
+                new SurroundModeCommand { Value = "?" },
+                new TunerFrequencyCommand { Value = "?" },
+                new TunerModeCommand { Value = "?" },
+                new ZonePowerCommand { Value = "?", ZoneId = 2 },
+                new ZoneVolumeCommand { Value = "?", ZoneId = 2 },
+                new ZoneMuteCommand { Value = "?", ZoneId = 2 },
+                new ZoneInputCommand { Value = "?", ZoneId = 2 }
+            };
+
+            // Run all queries and let the event handler publish out the query results
+            foreach (var command in commands)
+            {
+                await _client.SendCommandAsync(command)
+                    .ConfigureAwait(false);
+            }
+        }
+
+        #endregion
+
+        #region IDisposable Support
+
+        /// <summary>
+        /// Dispose implementation.
+        /// </summary>
+        /// <param name="disposing">Indicates if disposing.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+            {
+                if (_client != null)
+                {
+                    _client.Dispose();
+                }
+            }
+
+            _disposed = true;
+            base.Dispose(disposing);
+        }
+
+        #endregion
+    }
+}
---
+++ src/HomeAutio.Mqtt.Neato/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace HomeAutio.Mqtt.Neato
+{
+    /// <summary>
+    /// Main program entry point.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Main program entry point.
+        /// </summary>
+        /// <param name="args">Arguments.</param>
+        public static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        /// <summary>
+        /// Main program entry point.
+        /// </summary>
+        /// <param name="args">Arguments.</param>
+        /// <returns>Awaitable <see cref="Task" />.</returns>
+        public static async Task MainAsync(string[] args)
+        {
+            var environmentName = Environment.GetEnvironmentVariable("ENVIRONMENT");
+            if (string.IsNullOrEmpty(environmentName))
+                environmentName = "Development";
+
+            // Setup config
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Environment.CurrentDirectory)
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddJsonFile($"appsettings.{environmentName}.json", optional: true)
+                .Build();
+
+            // Setup logging
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(config)
+                .CreateLogger();
+
+            try
+            {
+                var hostBuilder = CreateHostBuilder(config);
+                await hostBuilder.RunConsoleAsync();
+            }
+            catch (Exception ex)
+            {
+                Log.Logger.Fatal(ex, ex.Message);
+                throw;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        /// <summary>
+        /// Creates an <see cref="IHostBuilder"/>.
+        /// </summary>
+        /// <param name="config">External configuration.</param>
+        /// <returns>A configured <see cref="IHostBuilder"/>.</returns>
+        private static IHostBuilder CreateHostBuilder(IConfiguration config)
+        {
+            return new HostBuilder()
+                .ConfigureAppConfiguration((hostContext, configuration) => configuration.AddConfiguration(config))
+                .ConfigureLogging((hostingContext, logging) => logging.AddSerilog())
+                .ConfigureServices((hostContext, services) =>
+                {
+                    // Setup client
+                    services.AddScoped<IClient>(serviceProvider =>
+                    {
+                        if (config.GetValue<string>("denon:denonConnectionType") == "http")
+                        {
+                            return new I8Beef.Denon.HttpClient.Client(config.GetValue<string>("denon:denonHost"));
+                        }
+                        else
+                        {
+                            return new I8Beef.Denon.TelnetClient.Client(config.GetValue<string>("denon:denonHost"));
+                        }
+                    });
+
+                    // Setup service instance
+                    services.AddScoped<IHostedService, NeatoMqttService>(serviceProvider =>
+                    {
+                        var brokerSettings = new Core.BrokerSettings
+                        {
+                            BrokerIp = config.GetValue<string>("mqtt:brokerIp"),
+                            BrokerPort = config.GetValue<int>("mqtt:brokerPort"),
+                            BrokerUsername = config.GetValue<string>("mqtt:brokerUsername"),
+                            BrokerPassword = config.GetValue<string>("mqtt:brokerPassword"),
+                            BrokerUseTls = config.GetValue<bool>("mqtt:brokerUseTls", false)
+                        };
+
+                        // TLS settings
+                        if (brokerSettings.BrokerUseTls)
+                        {
+                            var brokerTlsSettings = new Core.BrokerTlsSettings
+                            {
+                                AllowUntrustedCertificates = config.GetValue<bool>("mqtt:brokerTlsSettings:allowUntrustedCertificates", false),
+                                IgnoreCertificateChainErrors = config.GetValue<bool>("mqtt:brokerTlsSettings:ignoreCertificateChainErrors", false),
+                                IgnoreCertificateRevocationErrors = config.GetValue<bool>("mqtt:brokerTlsSettings:ignoreCertificateRevocationErrors", false)
+                            };
+
+                            switch (config.GetValue<string>("mqtt:brokerTlsSettings:protocol", "1.2"))
+                            {
+                                case "1.0":
+                                    brokerTlsSettings.SslProtocol = System.Security.Authentication.SslProtocols.Tls;
+                                    break;
+                                case "1.1":
+                                    brokerTlsSettings.SslProtocol = System.Security.Authentication.SslProtocols.Tls11;
+                                    break;
+                                case "1.2":
+                                default:
+                                    brokerTlsSettings.SslProtocol = System.Security.Authentication.SslProtocols.Tls12;
+                                    break;
+                            }
+
+                            var brokerTlsCertificatesSection = config.GetSection("mqtt:brokerTlsSettings:certificates");
+                            brokerTlsSettings.Certificates = brokerTlsCertificatesSection.GetChildren()
+                                .Select(x =>
+                                {
+                                    var file = x.GetValue<string>("file");
+                                    var passPhrase = x.GetValue<string>("passPhrase");
+
+                                    if (!File.Exists(file))
+                                        throw new FileNotFoundException($"Broker Certificate '{file}' is missing!");
+
+                                    return !string.IsNullOrEmpty(passPhrase) ?
+                                        new X509Certificate2(file, passPhrase) :
+                                        new X509Certificate2(file);
+                                }).ToList();
+
+                            brokerSettings.BrokerTlsSettings = brokerTlsSettings;
+                        }
+
+                        return new NeatoMqttService(
+                            serviceProvider.GetRequiredService<ILogger<NeatoMqttService>>(),
+                            serviceProvider.GetRequiredService<IRobot>(),
+                            config.GetValue<string>("denon:denonName"),
+                            brokerSettings);
+                    });
+                });
+        }
+    }
+}
